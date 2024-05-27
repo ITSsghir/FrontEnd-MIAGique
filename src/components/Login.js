bc3b2ef0851e2675b96be25e40043884dcec0f22
Younest9 <y.elarjouni.sq@gmail.com>
2024-05-27 23:22:07 +0200

    feat: Add login functionality with authentication handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,37 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
-    navigate('/signup');
+    try {
+      // Use axios for making the HTTP request
+      const response = await axios.post('http://localhost:8080/login', {
+        email: email,
+        password: password,
+      });
+
+      console.log(response);
+      
+      // Extract the sessionID from the response headers
+      const sessionID = response.headers['session-id'];
+      console.log(sessionID);
+
+      if (response.status !== 200) {
+        throw new Error('Authentication failed');
+      }
+      setIsLoggedIn(true); // Set authentication status to true
+      navigate('/spectatorHome'); // Redirect to authenticated route
+    } catch (error) {
+      console.error('Authentication error:', error);
+      // Handle authentication failure (e.g., display error message)
+    }
   };
 
   return (
