cf4b8672b78e0fa5ea50e96909ac2f3b2635a984
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 00:42:49 +0200

    Remove unnecessary routes in App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Home from './components/Home';
 import EventsList from './components/EventsList';
 import Payment from './components/Payment';
 import Tickets from './components/Tickets';
-import ParticipantHome from './components/ParticipantHome';
-import EventsListParticipant from './components/EventsListParticipant';
 import ResultsAndRankings from './components/ResultsAndRankings';
 import ControllerHome from './components/ControllerHome';
 
@@ -21,8 +19,6 @@ const App = () => {
         <Route path="/events-list" element={<EventsList />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/tickets" element={<Tickets />} />
-        <Route path="/participant-home" element={<ParticipantHome />} />
-        <Route path="/events-list-participant" element={<EventsListParticipant />} />
         <Route path="/results-and-rankings" element={<ResultsAndRankings />} />
         <Route path="/controller-home" element={<ControllerHome />} />
       </Routes>
