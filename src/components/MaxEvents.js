08a659ff80bbf0d18194bb312c4ba03dcf829e7c
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 17:45:29 +0200

    chore: Update MaxEvents component to display success/error messages for setting maximum number of events

--- src/components/MaxEvents.js
+++ src/components/MaxEvents.js
@@ -1,43 +1,77 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 
 const MaxEvents = () => {
-  const [maxEvents, setMaxEvents] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageColor, setMessageColor] = useState('green');
+  const { epreuves, updateEpreuve } = useAuth();
+  const [epreuveName, setEpreuveName] = useState('');
+  const [epreuve, setEpreuve] = useState(null);
+  const [availableSeats, setAvailableSeats] = useState(0);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:8080/api/events/max', {
-        maxEvents
-      });
-      alert('Nombre maximal d\'épreuves défini avec succès');
-    } catch (error) {
+      console.log('Applying max events', epreuve, epreuve.id, availableSeats);
+      const infrastructureCapacity = epreuve.infrastructure.capacite;
+      if (availableSeats > infrastructureCapacity) {
+        setMessage('Le nombre maximal d\'épreuves ne peut pas dépasser la capacité de l\'infrastructure');
+        setMessageColor('red');
+        return;
+      }
+
+      await updateEpreuve(epreuve.id, epreuve.nom, epreuve.date, epreuve.infrastructure, availableSeats);
+
+      setMessage('Nombre maximal d\'épreuves défini avec succès');
+      setMessageColor('green');
+    }
+    catch (error) {
       console.error('Erreur lors de la définition du nombre maximal d\'épreuves', error);
-      alert('Erreur lors de la définition du nombre maximal d\'épreuves');
+      setMessage('Erreur lors de la définition du nombre maximal d\'épreuves');
+      setMessageColor('red');
     }
   };
 
-  const handleLogout = () => {
-    navigate('/login');
+  const handleBack = () => {
+    navigate('/');
   };
 
-  const handleBack = () => {
-    navigate('/organizer-home');
-  };
+  useEffect(() => {
+    if (epreuve) {
+      setAvailableSeats(epreuve.nombrePlaces);
+    }
+  }, [epreuve]);
 
   return (
     <div className="form-container">
       <header className="form-header">
         <h1>MIAGique</h1>
         <button className="back" onClick={handleBack}>Retour</button>
-        <button className="logout" onClick={handleLogout}>Logout</button>
       </header>
-      <h2>Définir le Nombre Maximal d'Épreuves</h2>
+      <h2>Définir le Nombre Maximal de places</h2>
+      <p style={{ color: messageColor }}>{message}</p>
       <form onSubmit={handleSubmit}>
         <label>Nombre Maximal d'Épreuves:</label>
-        <input type="number" value={maxEvents} onChange={(e) => setMaxEvents(e.target.value)} required />
+        {/*Dropdown list of epreuves and when i select an epreuve, i want to display the number of availableSeats (availableSeats is a number) */}
+        <select value={epreuveName} onChange={(e) => {
+          setEpreuveName(e.target.value);
+          if (!e.target.value) {
+            setAvailableSeats(0);
+            return;
+          }
+          const epreuve = epreuves.find((epreuve) => epreuve.nom === e.target.value);
+          setEpreuve(epreuve);
+          setAvailableSeats(epreuve.nombrePlaces);
+        }} required>
+          <option value="">Choisissez une épreuve</option>
+          {epreuves.map(epreuve => (
+            <option key={epreuve.id} value={epreuve.nom}>{epreuve.nom}</option>
+          ))}
+        </select>
+        <label>Nombre de places disponibles à la vente:</label>
+        <input type="number" value={availableSeats} onChange={(e) => setAvailableSeats(e.target.value)} required />
         <button type="submit">Valider</button>
       </form>
     </div>
