bdf73145558b79b3a74afa37b483cfb157d74d5e
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 17:45:24 +0200

    refactor: Update ManageParticipants component to use authentication context and display success/error messages for participant removal

--- src/components/ManageParticipants.js
+++ src/components/ManageParticipants.js
@@ -3,10 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
 const ManageParticipants = () => {
-  const { participants, deleteParticipant } = useAuth();
+  const { participants, deleteParticipant, getParticipants, updateParticipant, delegations } = useAuth();
+  const [participant, setParticipant] = useState({});
+  const [delegationName, setDelegationName] = useState('');
   const [messageColor, setMessageColor] = useState('green');
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
+  const [editModeParticipantId, setEditModeParticipantId] = useState(null); // Track participant in edit mode
+
+  useEffect(() => {
+    getParticipants();
+  }, [getParticipants]);
+
   const handleRemove = async (id) => {
     try {
       await deleteParticipant(id);
@@ -18,19 +26,32 @@ const ManageParticipants = () => {
       setMessageColor('red');
     }
   };
+
+  const handleUpdate = async (id) => {
+    try {
+      const delegation = delegations.find(delegation => delegation.nom === delegationName);
+      await updateParticipant(id, participant.prenom, participant.nom, participant.email, delegation.id);
+      setMessage('Participant modifié avec succès');
+      setMessageColor('green');
+      setEditModeParticipantId(null); // Exit edit mode
+    } catch (error) {
+      console.error('Erreur lors de la modification du participant', error);
+      setMessage('Erreur lors de la modification du participant');
+      setMessageColor('red');
+    }
+  };
+
   const handleBack = () => {
     navigate('/');
   };
 
-  useEffect(() => {
-    console.log('Participants', participants);
-    for (let participant of participants) {
-      console.log('Participant', participant);
-      console.log("delegation participant", participant.delegation);
-    }
-  }
-  , [participants]);
+  const toggleEditMode = (id) => {
+    setEditModeParticipantId(id === editModeParticipantId ? null : id); // Toggle edit mode
+  };
 
+  const isEditMode = (participantId) => {
+    return editModeParticipantId === participantId;
+  };
 
   return (
     <div className="table-container">
@@ -40,7 +61,6 @@ const ManageParticipants = () => {
       </header>
       <h2>Gérer les Participants</h2>
       <p style={{ color: messageColor }}>{message}</p>
-      {'\n'}
       <table>
         <thead>
           <tr>
@@ -53,16 +73,51 @@ const ManageParticipants = () => {
           </tr>
         </thead>
         <tbody>
-          {/* Display participants by accessing the participants array */}
           {participants.map(participant => (
             <tr key={participant.id}>
               <td>{participant.id}</td>
-              <td>{participant.prenom}</td>
-              <td>{participant.nom}</td>
-              <td>{participant.email}</td>
-              <td>{participant.delegation ? participant.delegation.id : 'N/A'}</td>
               <td>
-                <button style={{backgroundColor: "red"}} onClick={() => handleRemove(participant.id)}>Supprimer</button>
+                {isEditMode(participant.id) ? (
+                  <input type="text" value={participant.prenom} onChange={(e) => setParticipant({ ...participant, prenom: e.target.value })} />
+                ) : (
+                  participant.prenom
+                )}
+              </td>
+              <td>
+                {isEditMode(participant.id) ? (
+                  <input type="text" value={participant.nom} onChange={(e) => setParticipant({ ...participant, nom: e.target.value })} />
+                ) : (
+                  participant.nom
+                )}
+              </td>
+              <td>
+                {isEditMode(participant.id) ? (
+                  <input type="email" value={participant.email} onChange={(e) => setParticipant({ ...participant, email: e.target.value })} />
+                ) : (
+                  participant.email
+                )}
+              </td>
+              <td>
+                {isEditMode(participant.id) ? (
+                  <select value={delegationName} onChange={(e) => setDelegationName(e.target.value)} required>
+                    <option value="">{participant.delegation.nom}</option>
+                    {delegations.map(delegation => (
+                      delegation.nom !== participant.delegation.nom && (
+                        <option key={delegation.id} value={delegation.nom}>{delegation.nom}</option>
+                      )
+                    ))}
+                  </select>
+                ) : (
+                  participant.delegation.nom
+                )}
+              </td>
+              <td>
+                {isEditMode(participant.id) ? (
+                  <button onClick={() => handleUpdate(participant.id)}>Valider</button>
+                ) : (
+                  <button onClick={() => toggleEditMode(participant.id)}>Modifier</button>
+                )}
+                <button style={{ backgroundColor: "red" }} onClick={() => handleRemove(participant.id)}>Supprimer</button>
               </td>
             </tr>
           ))}
