914af8874aef243da1f6d317ec2e936bc8190f74
Younest9 <y.elarjouni.sq@gmail.com>
2024-06-16 21:34:56 +0200

    refactor: Update routing in App.js to use Result component instead of deprecated components

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ import EventsList from './components/EventsList';
 import Payment from './components/Payment';
 import Tickets from './components/Tickets';
 import ResultsAndRankings from './components/ResultsAndRankings';
-import ControllerHome from './components/ControllerHome';
-import AddDelegation from './components/AddDelegation';
-import RemoveDelegation from './components/RemoveDelegation';
-import AddParticipant from './components/AddParticipant';
-import MaxParticipants from './components/MaxParticipants';
-import ManageParticipants from './components/ManageParticipants';
-import AddEvent from './components/AddEvent';
-import MaxEvents from './components/MaxEvents';
-import ManageEvents from './components/ManageEvents';
-import AddController from './components/AddController';
-import ManageControllers from './components/ManageControllers';
+import Result from './components/Result';
 
 const App = () => {
   return (
@@ -30,17 +20,7 @@ const App = () => {
         <Route path="/payment" element={<Payment />} />
         <Route path="/tickets" element={<Tickets />} />
         <Route path="/results-and-rankings" element={<ResultsAndRankings />} />
-        <Route path="/controller-home" element={<ControllerHome />} />
-        <Route path="/add-delegation" element={<AddDelegation />} />
-        <Route path="/remove-delegation" element={<RemoveDelegation />} />
-        <Route path="/add-participant" element={<AddParticipant />} />
-        <Route path="/max-participants" element={<MaxParticipants />} />
-        <Route path="/manage-participants" element={<ManageParticipants />} />
-        <Route path="/add-event" element={<AddEvent />} />
-        <Route path="/max-events" element={<MaxEvents />} />
-        <Route path="/manage-events" element={<ManageEvents />} />
-        <Route path="/add-controller" element={<AddController />} />
-        <Route path="/manage-controllers" element={<ManageControllers />} />
+        <Route path="/results" element={<Result />} />
       </Routes>
     </Router>
   );
